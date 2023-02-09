ab039940ec8eafeaadef90c4d8cea249e71ee5c5
Sergio Archila <archilargosergio@gmail.com>
2023-02-08 21:59:48 -0500

    [ADD] firestoreConfig file

---
+++ src/firebaseConfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+// import { getFirestore, collection } from 'firebase/firebase'; 
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBGVznYSiX943hFHjNyAxCDxctRZgxil34",
+  authDomain: "e-commerce-coder-house-b44fa.firebaseapp.com",
+  projectId: "e-commerce-coder-house-b44fa",
+  storageBucket: "e-commerce-coder-house-b44fa.appspot.com",
+  messagingSenderId: "857909865973",
+  appId: "1:857909865973:web:c90e155cba8aae654f84bc"
+};
+
+export const app = initializeApp(firebaseConfig);
+// const db = getFirestore(app);
